33a8ddf553ad96a26ea771e1a95d392eb84c02b7
m-tkachenko <tk.mykhailo@gmail.com>
2020-07-08 17:38:54 +0300

    Added: Buttons in MultiplyLearnActivity and BottomSheet

--- app/src/main/java/com/salo/pockettrachtenberg/MethodsActivity.kt
+++ app/src/main/java/com/salo/pockettrachtenberg/MethodsActivity.kt
@@ -3,9 +3,18 @@ package com.salo.pockettrachtenberg
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.activity_methods.*
+import kotlinx.android.synthetic.main.activity_multiply_learn.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
 class MethodsActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_methods)
@@ -25,5 +34,6 @@ class MethodsActivity : AppCompatActivity() {
         id_cardview_material_squaring.setOnClickListener {
             startActivity(Intent(this, SquaringLearnActivity::class.java))
         }
+
     }
 }
--- app/src/main/java/com/salo/pockettrachtenberg/MultiplyLearnActivity.kt
+++ app/src/main/java/com/salo/pockettrachtenberg/MultiplyLearnActivity.kt
@@ -2,10 +2,29 @@ package com.salo.pockettrachtenberg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_multiply_learn.*
+import kotlinx.android.synthetic.main.bottom_sheet.*
 
 class MultiplyLearnActivity : AppCompatActivity() {
+
+    private lateinit var sheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_multiply_learn)
+
+        sheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+        sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
+
+        id_multiply6_button.setOnClickListener {
+            if(sheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN) {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+            }
+            else {
+                sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN)
+            }
+        }
     }
 }
